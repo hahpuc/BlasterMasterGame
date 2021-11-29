87f07919753904d6924ffc823ed69ac71577e2f0
Nguyen Thanh Long <18520092@gm.uit.edu.vn>
2021-11-29 23:18:55 +0700

    Change location

--- 05-ScenceManager/main.cpp
+++ 05-ScenceManager/main.cpp
@@ -13,8 +13,8 @@
 
 #include "PlayScence.h"
 
-#define WINDOW_CLASS_NAME L"SampleWindow"
-#define MAIN_WINDOW_TITLE L"SAMPLE 05 - SCENCE MANAGER"
+#define WINDOW_CLASS_NAME L"Blaster Master"
+#define MAIN_WINDOW_TITLE L"Blaster Master"
 
 #define BACKGROUND_COLOR D3DCOLOR_XRGB(255, 255, 200)
 #define SCREEN_WIDTH 400
